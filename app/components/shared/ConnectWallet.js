c33d83b34ad74d3a4574931dc5a4a31a74cbf4b5
Brad <klausmickelson52@gmail.com>
2024-08-22 22:21:44 +0100

    feat: create token

--- app/components/shared/ConnectWallet.js
+++ app/components/shared/ConnectWallet.js
@@ -5,109 +5,109 @@ import Image from "next/image";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 export const ConnectWallet = ({ children }) => {
-    return (
-        <ConnectButton.Custom>
-            {({
-                account,
-                chain,
-                openAccountModal,
-                openChainModal,
-                openConnectModal,
-                authenticationStatus,
-                mounted,
-            }) => {
-                // Note: If your app doesn't use authentication, you
-                // can remove all 'authenticationStatus' checks
-                const ready = mounted && authenticationStatus !== "loading";
-                const connected =
-                    ready &&
-                    account &&
-                    chain &&
-                    (!authenticationStatus || authenticationStatus === "authenticated");
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
                 return (
-                    <div
-                        {...(!ready && {
-                            "aria-hidden": true,
-                            style: {
-                                opacity: 0,
-                                pointerEvents: "none",
-                                userSelect: "none",
-                            },
-                        })}
-                    >
-                        {(() => {
-                            if (!connected) {
-                                return (
-                                    <button
-                                        onClick={openConnectModal}
-                                        type="button"
-                                        className="w-full"
-                                    >
-                                        {children}
-                                    </button>
-                                );
-                            }
-                            if (chain.unsupported) {
-                                return (
-                                    <button
-                                        onClick={openChainModal}
-                                        type="button"
-                                        className="text-white text-[0.625rem] md:text-xs whitespace-nowrap border w-full border-neutral-800 rounded-2xl md:px-4 p-2 md:py-[0.6875rem] bg-eerie-black-3"
-                                    >
-                                        Wrong network
-                                    </button>
-                                );
-                            }
-                            return (
-                                <div
-                                    style={{ display: "flex", gap: 10, alignItems: "center" }}
-                                    className="text-white text-[0.625rem] md:text-xs border md:px-4 bg-eerie-black-3 rounded-2xl border-neutral-800 p-[0.375rem] md:py-[0.6875rem]"
-                                >
-                                    <button
-                                        onClick={openChainModal}
-                                        style={{
-                                            display: chain.hasIcon ? "flex" : "none",
-                                            alignItems: "center",
-                                        }}
-                                        type="button"
-                                    >
-                                        {chain.hasIcon && (
-                                            <div
-                                                style={{
-                                                    background: chain.iconBackground,
-                                                    width: 26,
-                                                    height: 26,
-                                                    borderRadius: 999,
-                                                    overflow: "hidden",
-                                                }}
-                                            >
-                                                {chain.iconUrl && (
-                                                    <Image
-                                                        alt={chain.name ?? "Chain icon"}
-                                                        src={chain.iconUrl}
-                                                        width={26}
-                                                        height={26}
-                                                    />
-                                                )}
-                                            </div>
-                                        )}
-                                    </button>
-                                    <button
-                                        onClick={openAccountModal}
-                                        type="button"
-                                        className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[6rem]"
-                                    >
-                                        {account.displayName}
-                                    </button>
-                                    <span>
-                                        <MdKeyboardArrowDown size={18} />
-                                    </span>
-                                </div>
-                            );
-                        })()}
-                    </div>
+                  <button
+                    onClick={openConnectModal}
+                    type="button"
+                    className="w-full"
+                  >
+                    {children}
+                  </button>
                 );
-            }}
-        </ConnectButton.Custom>
-    );
+              }
+              if (chain.unsupported) {
+                return (
+                  <button
+                    onClick={openChainModal}
+                    type="button"
+                    className="text-white text-[0.625rem] md:text-xs whitespace-nowrap border w-full border-neutral-800 rounded-2xl md:px-4 p-2 md:py-[0.6875rem] bg-eerie-black-3"
+                  >
+                    Wrong network
+                  </button>
+                );
+              }
+              return (
+                <div
+                  style={{ display: "flex", gap: 10, alignItems: "center" }}
+                  className="text-white text-[0.625rem] md:text-xs border md:px-4 bg-eerie-black-3 rounded-2xl border-neutral-800 p-[0.375rem] md:py-[0.6875rem]"
+                >
+                  <button
+                    onClick={openChainModal}
+                    style={{
+                      display: chain.hasIcon ? "flex" : "none",
+                      alignItems: "center",
+                    }}
+                    type="button"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 26,
+                          height: 26,
+                          borderRadius: 999,
+                          overflow: "hidden",
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <Image
+                            alt={chain.name ?? "Chain icon"}
+                            src={chain.iconUrl}
+                            width={26}
+                            height={26}
+                          />
+                        )}
+                      </div>
+                    )}
+                  </button>
+                  <button
+                    onClick={openAccountModal}
+                    type="button"
+                    className="whitespace-nowrap overflow-hidden text-ellipsis max-w-[6rem]"
+                  >
+                    {account.displayName}
+                  </button>
+                  <span>
+                    <MdKeyboardArrowDown size={18} />
+                  </span>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
 };
--- app/providers/ethers.js
+++ app/providers/ethers.js
@@ -2,19 +2,19 @@ import { Config, getConnectorClient } from "@wagmi/core";
 import { BrowserProvider, JsonRpcSigner } from "ethers";
 
 export function clientToSigner(client) {
-    const { account, chain, transport } = client;
-    const network = {
-        chainId: chain.id,
-        name: chain.name,
-        ensAddress: chain.contracts?.ensRegistry?.address,
-    };
-    const provider = new BrowserProvider(transport, network);
-    const signer = new JsonRpcSigner(provider, account.address);
-    return signer;
+  const { account, chain, transport } = client;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new BrowserProvider(transport, network);
+  const signer = new JsonRpcSigner(provider, account.address);
+  return signer;
 }
 
 /** Action to convert a viem Wallet Client to an ethers.js Signer. */
-export async function getEthersSigner(config, { chainId }) {
-    const client = await getConnectorClient(config, { chainId });
-    return clientToSigner(client);
+export async function getEthersSigner(config, { chainId } = {}) {
+  const client = await getConnectorClient(config, { chainId });
+  return clientToSigner(client);
 }
