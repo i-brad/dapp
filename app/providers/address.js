7fa3936115a147c1a4fdbd4d3875412a77b50ce1
new dapp sync <oluwafemialofe@yahoo.com>
2024-08-24 01:20:24 +0400

    Redeploye token factory to fix issues with token decimals other than 18

--- app/providers/address.js
+++ app/providers/address.js
@@ -1,2 +1,2 @@
-export const TokenFactory = "0xf5B15a5a64301cFcc39c90317EBD6Aa3a22cF144";
+export const TokenFactory = "0xf6ece2d79947f1e2f13617f8ed4041468c3acbcc";
 export const LockFactory = "0xe1B2a006271D9cBDF2561091FafF7E23281Eefe7";
