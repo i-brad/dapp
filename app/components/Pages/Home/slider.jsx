19c286a614602fce590069db6ea243a1f2b7dac6
new dapp sync <oluwafemialofe@yahoo.com>
2024-08-30 03:47:52 +0400

    Add link to docs

--- app/components/Pages/Home/slider.jsx
+++ app/components/Pages/Home/slider.jsx
@@ -10,104 +10,105 @@ import "swiper/css/pagination";
 import "./slider.css";
 
 const MySwiperComponent = () => {
-  return (
-    <Swiper
-      // pagination={true}
-      spaceBetween={30}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination, Navigation]}
-      navigation={true}
-      loop={true}
-      className="mySwiper h-80 rounded-lg"
-    >
-      <SwiperSlide>
-        <div
-          className="h-full px-4 md:px-12 lg:px-24 rounded-lg"
-          style={{
-            backgroundImage: `url('/images/e7ec75ea0dc3c2bc8fc9b3327a9d7467.jpeg')`,
-          }}
+    return (
+        <Swiper
+            // pagination={true}
+            spaceBetween={30}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination, Navigation]}
+            navigation={true}
+            loop={true}
+            className="mySwiper h-80 rounded-lg"
         >
-          <div className="h-full flex flex-col items-start justify-center">
-            <div className="space-y-2 z-10">
-              <h1 className="font-semibold text-3xl text-white">
-                Fair Token Launchpad and Token Management Protocol
-              </h1>
-              <p className="text-white font-lg">
-                Thrustpad allows projects organize raise for initial Liquidity,
-                setup staking, create locks and launch token in one place.
-              </p>
-            </div>
-            <div className="relative mt-8 flex flex-col items-start justify-start gap-2">
-              <div className="flex items-center justify-between text-base z-10">
-                <Link
-                  className="bg-[#DA5921] border border-[#DA5921] text-[#fff] px-5 py-3 text-base rounded-lg"
-                  href="/fair-launch/create"
+            <SwiperSlide>
+                <div
+                    className="h-full px-4 md:px-12 lg:px-24 rounded-lg"
+                    style={{
+                        backgroundImage: `url('/images/e7ec75ea0dc3c2bc8fc9b3327a9d7467.jpeg')`,
+                    }}
                 >
-                  Create now
-                </Link>
-                <div className="flex items-center">
-                  <Link
-                    href="/"
-                    className="inline-block w-auto px-2 py-2.5 font-semibold"
-                  >
-                    <span className="border-b-2 border-gray-300">
-                      Read Docs
-                    </span>
-                  </Link>
+                    <div className="h-full flex flex-col items-start justify-center">
+                        <div className="space-y-2 z-10">
+                            <h1 className="font-semibold text-3xl text-white">
+                                Fair Token Launchpad and Token Management Protocol
+                            </h1>
+                            <p className="text-white font-lg">
+                                Thrustpad allows projects organize raise for initial Liquidity,
+                                setup staking, create locks and launch token in one place.
+                            </p>
+                        </div>
+                        <div className="relative mt-8 flex flex-col items-start justify-start gap-2">
+                            <div className="flex items-center justify-between text-base z-10">
+                                <Link
+                                    className="bg-[#DA5921] border border-[#DA5921] text-[#fff] px-5 py-3 text-base rounded-lg"
+                                    href="/fair-launch/create"
+                                >
+                                    Create now
+                                </Link>
+                                <div className="flex items-center">
+                                    <Link
+                                        target="_blank"
+                                        href="https://thrustpad.gitbook.io/docs"
+                                        className="inline-block w-auto px-2 py-2.5 font-semibold"
+                                    >
+                                        <span className="border-b-2 border-gray-300">
+                                            Read Docs
+                                        </span>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-[#31110282] pointer-events-none absolute top-0 left-0 w-full h-full opacity-60 z-[1]"></div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-[#31110282] pointer-events-none absolute top-0 left-0 w-full h-full opacity-60 z-[1]"></div>
-        </div>
-      </SwiperSlide>
+            </SwiperSlide>
 
-      <SwiperSlide>
-        <div
-          className="h-full px-4 md:px-12 lg:px-24 rounded-lg"
-          style={{
-            backgroundImage: `url('/images/e7ec75ea0dc3c2bc8fc9b3327a9d7467.jpeg')`,
-          }}
-        >
-          <div className="h-full flex flex-col items-start justify-center">
-            <div className="space-y-2 z-10">
-              <h1 className="font-semibold text-3xl text-white">
-                Fair Token Launchpad and Token Management Protocol2
-              </h1>
-              <p className="text-white font-lg">
-                Thrustpad allows projects organize raise for initial Liquidity,
-                setup staking, create locks and launch token in one place.
-              </p>
-            </div>
-            <div className="relative mt-8 flex flex-col items-start justify-start gap-2">
-              <div className="flex items-center justify-between text-base z-10">
-                <Link
-                  className="bg-[#DA5921] border border-[#DA5921] text-[#fff] px-5 py-3 text-base rounded-lg"
-                  href="/"
+            <SwiperSlide>
+                <div
+                    className="h-full px-4 md:px-12 lg:px-24 rounded-lg"
+                    style={{
+                        backgroundImage: `url('/images/e7ec75ea0dc3c2bc8fc9b3327a9d7467.jpeg')`,
+                    }}
                 >
-                  Create now
-                </Link>
-                <div className="flex items-center">
-                  <Link
-                    target="_blank"
-                    href="https://thrustpad.gitbook.io/docs"
-                    className="inline-block w-auto px-2 py-2.5 font-semibold"
-                  >
-                    <span className="border-b-2 border-gray-300">
-                      Read Docs
-                    </span>
-                  </Link>
+                    <div className="h-full flex flex-col items-start justify-center">
+                        <div className="space-y-2 z-10">
+                            <h1 className="font-semibold text-3xl text-white">
+                                Fair Token Launchpad and Token Management Protocol2
+                            </h1>
+                            <p className="text-white font-lg">
+                                Thrustpad allows projects organize raise for initial Liquidity,
+                                setup staking, create locks and launch token in one place.
+                            </p>
+                        </div>
+                        <div className="relative mt-8 flex flex-col items-start justify-start gap-2">
+                            <div className="flex items-center justify-between text-base z-10">
+                                <Link
+                                    className="bg-[#DA5921] border border-[#DA5921] text-[#fff] px-5 py-3 text-base rounded-lg"
+                                    href="/"
+                                >
+                                    Create now
+                                </Link>
+                                <div className="flex items-center">
+                                    <Link
+                                        target="_blank"
+                                        href="https://thrustpad.gitbook.io/docs"
+                                        className="inline-block w-auto px-2 py-2.5 font-semibold"
+                                    >
+                                        <span className="border-b-2 border-gray-300">
+                                            Read Docs
+                                        </span>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-[#31110282] pointer-events-none absolute top-0 left-0 w-full h-full opacity-60 z-[1]"></div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-[#31110282] pointer-events-none absolute top-0 left-0 w-full h-full opacity-60 z-[1]"></div>
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  );
+            </SwiperSlide>
+        </Swiper>
+    );
 };
 
 export default MySwiperComponent;
